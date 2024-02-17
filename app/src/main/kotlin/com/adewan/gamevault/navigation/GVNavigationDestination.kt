797ee62265fa0a3df7715158ad573b6fb412d82c
Abhishek Dewan <abhishekdewan2802@gmail.com>
2024-02-16 22:39:49 -0800

    UI + Navigation - Trying to scope in the scope creep

--- app/src/main/kotlin/com/adewan/gamevault/navigation/GVNavigationDestination.kt
+++ app/src/main/kotlin/com/adewan/gamevault/navigation/GVNavigationDestination.kt
@@ -1,16 +1,11 @@
 package com.adewan.gamevault.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import com.adewan.gamevault.R
-import com.adewan.gamevault.features.discover.DiscoverView
-import com.adewan.gamevault.features.search.SearchView
 
 data class GVBottomNavData(val label: String, val icon: @Composable () -> Painter)
 
@@ -26,19 +21,7 @@ val defaultNavigationDestinations =
       route = "discover",
       bottomNavData =
         GVBottomNavData(label = "Discover", icon = { painterResource(id = R.drawable.sparkles) }),
-      renderer = { navController ->
-        DiscoverView(
-          navigateToSearch = {
-            navController.navigate("search") { navigateBottomBar(navController) }
-          }
-        )
-      },
-    ),
-    GVNavigationDestination(
-      route = "search",
-      bottomNavData =
-        GVBottomNavData(label = "Search", icon = { rememberVectorPainter(Icons.Default.Search) }),
-      renderer = { SearchView() },
+      renderer = { navController -> },
     ),
     GVNavigationDestination(
       route = "vault",
